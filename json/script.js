ddbf7c43c1878cddedf7fddf60950d9fb9261327
wekonu <wahjoe.eko.n@gmail.com>
2021-02-21 14:28:32 +0700

    = using ajax getJSON

--- json/script.js
+++ json/script.js
@@ -6,13 +6,17 @@
 
 // console.log(JSON.stringify(mahasiswa));
 
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4 && xhr.status == 200){
-        let mahasiswa = JSON.parse(this.responseText);
-        console.log(mahasiswa);
-    }
-}
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if(xhr.readyState == 4 && xhr.status == 200){
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa);
+//     }
+// }
 
-xhr.open('GET', 'coba.json', true); // true - asyncly
-xhr.send();
+// xhr.open('GET', 'coba.json', true); // true - asyncly
+// xhr.send();
+
+$.getJSON('coba.json', function(data){
+    console.log(data);
+}); // stored as an object
