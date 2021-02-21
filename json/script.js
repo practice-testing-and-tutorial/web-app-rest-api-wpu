edb23989bf7523e4be38a36ea94b9320ddcebdc7
wekonu <wahjoe.eko.n@gmail.com>
2021-02-21 14:20:02 +0700

    - simple json parse

--- json/script.js
+++ json/script.js
@@ -1,7 +1,18 @@
-let mahasiswa = {
-    nama: "Wahyu",
-    nrp: "040305230",
-    email: "wahyu@mail.com"
+// let mahasiswa = {
+//     nama: "Wahyu",
+//     nrp: "040305230",
+//     email: "wahyu@mail.com"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status == 200){
+        let mahasiswa = JSON.parse(this.responseText);
+        console.log(mahasiswa);
+    }
 }
 
-console.log(JSON.stringify(mahasiswa));
+xhr.open('GET', 'coba.json', true); // true - asyncly
+xhr.send();
