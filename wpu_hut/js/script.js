ab712ea363f1836fdd602e71a5b7db81b4b57def
wekonu <wahjoe.eko.n@gmail.com>
2021-02-21 16:16:06 +0700

    +- decode using jQuery

---
+++ wpu_hut/js/script.js
@@ -0,0 +1,33 @@
+getData = function(i, data){
+    $('#daftar-menu').append(`<div class="col-md-4"><div class="card mb-3"><img src="img/pizza/${data.gambar}" class="card-img-top"><div class="card-body"><h5 class="card-title">${data.nama}</h5><p class="card-text">${data.deskripsi}</p><h5 class="card-title">IDR ${data.harga},-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>`);
+}
+
+createObject = function(data) {
+    let menu = data.menu;
+
+    $.each(menu, getData);
+}
+
+
+navOnClick = function() {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+
+    let cat = $(this).html();
+    $('h1').html(cat);
+
+    $.getJSON('data/pizza.json', function(data) {
+        let menus = data.menu;
+        let cont = '';
+        $.each(menus, function(i, data){
+            if(data.kategori == cat.toLowerCase()){
+                cont += `<div class="col-md-4"><div class="card mb-3"><img src="img/pizza/${data.gambar}" class="card-img-top"><div class="card-body"><h5 class="card-title">${data.nama}</h5><p class="card-text">${data.deskripsi}</p><h5 class="card-title">IDR ${data.harga},-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>`;
+            }
+        });
+
+        $('#daftar-menu').html(cont);
+    });
+}
+
+$.getJSON('data/pizza.json', createObject);
+$('.nav-link').on('click', navOnClick);
